1573e329d1337c0261bec71c33d82f4130acfa4c
Vitaly <itassan6@gmail.com>
2023-02-25 18:44:54 +0200

    Fixed bugs

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -1,101 +1,121 @@
-import styles from './Drawer.module.scss';
-import React from 'react';
-import axios from 'axios'
-import Info from '../Info';
-import { useCart } from '../hooks/useCart';
-
+import styles from "./Drawer.module.scss";
+import React from "react";
+import axios from "axios";
+import Info from "../Info";
+import { useCart } from "../../hooks/useCart";
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 function Drawer({ onClose, onRemove, items = [], opened }) {
-   const { cartItems, setCartItems, totalPrice, } = useCart()
-   const [OdrerId, setOrderId] = React.useState(null);
-   const [isOdrerComplite, setOrderCoplite] = React.useState(false);
-   const [isLoading, setIsLoading] = React.useState(false);
+  const { cartItems, setCartItems, totalPrice } = useCart();
+  const [OdrerId, setOrderId] = React.useState(null);
+  const [isOdrerComplite, setOrderCoplite] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(false);
 
+  const onClickOrder = async () => {
+    try {
+      setIsLoading(true);
+      const { data } = await axios.post(
+        `https://6273f31a345e1821b224d416.mockapi.io/orders`,
+        {
+          items: cartItems,
+        }
+      );
+      setOrderId(data.id);
+      setOrderCoplite(true);
+      setCartItems([]);
 
-   const onClickOrder = async () => {
-      try {
-         setIsLoading(true)
-         const { data } = await axios.post(`https://6273f31a345e1821b224d416.mockapi.io/orders`, {
-            items: cartItems,
-         });
-         setOrderId(data.id);
-         setOrderCoplite(true);
-         setCartItems([]);
-
-         for (let i = 0; i < cartItems.length; i++) {
-            const item = cartItems[i]
-            await axios.delete(`https://6273f31a345e1821b224d416.mockapi.io/cart/` + item.id);
-            await delay(1000);
-         }
-      } catch (error) {
-         alert('Не удалось создать заказ')
+      for (let i = 0; i < cartItems.length; i++) {
+        const item = cartItems[i];
+        await axios.delete(
+          `https://6273f31a345e1821b224d416.mockapi.io/cart/` + item.id
+        );
+        await delay(1000);
       }
-      setIsLoading(false)
-   };
+    } catch (error) {
+      alert("Не удалось создать заказ");
+    }
+    setIsLoading(false);
+  };
 
-   return (
-      <div className={`${styles.overlay} ${opened ? `${styles.overlayVisible}` : ""}`}>
-         <div className={styles.drawer}>
-            <h2>
-               Корзина
-               <img onClick={onClose} className={styles.removeBtn} src="img/btn-remove.svg" alt="Remove" />
-            </h2>
+  return (
+    <div
+      className={`${styles.overlay} ${
+        opened ? `${styles.overlayVisible}` : ""
+      }`}
+    >
+      <div className={styles.drawer}>
+        <h2>
+          Корзина
+          <img
+            onClick={onClose}
+            className={styles.removeBtn}
+            src="img/btn-remove.svg"
+            alt="Remove"
+          />
+        </h2>
 
-            {items.length > 0 ? (
-               <div className='d-flex flex-column flex' >
-                  <div className={styles.cartItems}  >
-                     {items.map((obj) => (
-                        <div key={obj.id} className={styles.cartItem} >
-                           <div
-                              style={{ backgroundImage: `url(${obj.imageUrl})` }}
-                              className={styles.cartItemImg}>
-                           </div>
-                           <div className="mr-20 flex" >
-                              <p className="mb-5" >{obj.title}</p>
-                              <b>{obj.price}грн.</b>
-                           </div>
-                           <img
-                              onClick={() => onRemove(obj.id)}
-                              className={styles.removeBtn}
-                              src="img/btn-remove.svg"
-                              alt="Remove" />
-                        </div>
-                     ))}
+        {items.length > 0 ? (
+          <div className="d-flex flex-column flex">
+            <div className={styles.cartItems}>
+              {items.map((obj) => (
+                <div key={obj.id} className={styles.cartItem}>
+                  <div
+                    style={{ backgroundImage: `url(${obj.imageUrl})` }}
+                    className={styles.cartItemImg}
+                  ></div>
+                  <div className="mr-20 flex">
+                    <p className="mb-5">{obj.title}</p>
+                    <b>{obj.price}грн.</b>
                   </div>
-                  <div className={styles.cartTotalBlock} >
-                     <ul >
-                        <li >
-                           <span>Итого:</span>
-                           <div>
-                           </div>
-                           <b>{totalPrice} грн.</b>
-                        </li>
-                        <li>
-                           <span >Налог 5%:</span>
-                           <div></div>
-                           <b> {totalPrice / 100 * 5} грн. </b>
-                        </li>
-                     </ul>
-                     <button disabled={isLoading} onClick={onClickOrder} className={styles.greenButton}>
-                        Оформить заказ
-                        <img src='img/arrow.svg' alt="arrow" />
-                     </button>
-                  </div>
-
-               </div>
-            ) : (
-               <Info
-                  image={isOdrerComplite ? 'img/complited-order.jpg' : 'img/empty-cart.png'}
-                  title={isOdrerComplite ? 'Заказ оформлен!' : 'Корзина пустая'}
-                  discription={isOdrerComplite ? `Ваш заказ №${OdrerId} скоро будет передан курьерской доставке` : 'Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.'}
-
-               />
-            )}
-         </div>
-      </div >
-   );
+                  <img
+                    onClick={() => onRemove(obj.id)}
+                    className={styles.removeBtn}
+                    src="img/btn-remove.svg"
+                    alt="Remove"
+                  />
+                </div>
+              ))}
+            </div>
+            <div className={styles.cartTotalBlock}>
+              <ul>
+                <li>
+                  <span>Итого:</span>
+                  <div></div>
+                  <b>{totalPrice} грн.</b>
+                </li>
+                <li>
+                  <span>Налог 5%:</span>
+                  <div></div>
+                  <b> {(totalPrice / 100) * 5} грн. </b>
+                </li>
+              </ul>
+              <button
+                disabled={isLoading}
+                onClick={onClickOrder}
+                className={styles.greenButton}
+              >
+                Оформить заказ
+                <img src="img/arrow.svg" alt="arrow" />
+              </button>
+            </div>
+          </div>
+        ) : (
+          <Info
+            image={
+              isOdrerComplite ? "img/complited-order.jpg" : "img/empty-cart.png"
+            }
+            title={isOdrerComplite ? "Заказ оформлен!" : "Корзина пустая"}
+            discription={
+              isOdrerComplite
+                ? `Ваш заказ №${OdrerId} скоро будет передан курьерской доставке`
+                : "Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ."
+            }
+          />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Drawer;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,36 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import header from './header.module.scss';
-import { useCart } from '../hooks/useCart';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import header from "./header.module.scss";
+import { useCart } from "../../hooks/useCart";
 
 function Header(props) {
-   const { totalPrice } = useCart()
-   return (
-      <header className={header}>
-         <Link to="">
-            <div className="d-flex align-center">
-               <img width={40} height={40} src="img/sneacers-logo.png" alt='sneacers-logo' />
-               <div >
-                  <h3 className="text-uppercase" >react sneacers</h3>
-                  <p className='opacity-5' >Магазин лучших кроссовок</p>
-               </div>
-            </div>
-         </Link>
-         <ul className="d-flex" >
-            <li onClick={props.onClickCart} className="mr-30 cu-p" >
-               <img width={18} height={18} src="img/cart.svg" alt='cartIcon' />
-               <span className='opacity-6 ml-15 '>{totalPrice} грн.</span>
-            </li>
-            <li className="cu-p">
-               <Link to="favorites"> <img width={18} height={18} src="img/favorites-page-icon.svg" alt='favoritesIcon' /></Link>
-            </li>
-            <li className="cu-p">
-               <Link to="orders"><img width={18} height={18} src="img/user.svg" alt='userIcon' /></Link>
-            </li>
-         </ul>
-      </header>
-   )
+  const { totalPrice } = useCart();
+  return (
+    <header className={header}>
+      <Link to="">
+        <div className="d-flex align-center">
+          <img
+            width={40}
+            height={40}
+            src="img/sneacers-logo.png"
+            alt="sneacers-logo"
+          />
+          <div>
+            <h3 className="text-uppercase">react sneacers</h3>
+            <p className="opacity-5">Магазин лучших кроссовок</p>
+          </div>
+        </div>
+      </Link>
+      <ul className="d-flex">
+        <li onClick={props.onClickCart} className="mr-30 cu-p">
+          <img width={18} height={18} src="img/cart.svg" alt="cartIcon" />
+          <span className="opacity-6 ml-15 ">{totalPrice} грн.</span>
+        </li>
+        <li className="cu-p">
+          <Link to="favorites">
+            {" "}
+            <img
+              width={18}
+              height={18}
+              src="img/favorites-page-icon.svg"
+              alt="favoritesIcon"
+            />
+          </Link>
+        </li>
+        <li className="cu-p">
+          <Link to="orders">
+            <img width={18} height={18} src="img/user.svg" alt="userIcon" />
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
